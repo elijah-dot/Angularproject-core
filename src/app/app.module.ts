838e407f0c81d49eb1274fb34e955b3692bdc4ca
Moringa School <wangu.mwangi@student.moringaschool.com>
2022-04-10 01:19:47 +0300

    created component quote-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuotesDetailsComponent } from './quotes-details/quotes-details.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     QuoteComponent,
-    QuotesDetailsComponent
+    QuotesDetailsComponent,
+    QuoteFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
