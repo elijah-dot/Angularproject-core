e15e96b5e2280594775a953cafc135b6eb4d3c1a
Moringa School <wangu.mwangi@student.moringaschool.com>
2022-04-10 01:03:06 +0300

    style:applied global styling to body

--- src/app/quotes-details/quotes-details.component.ts
+++ src/app/quotes-details/quotes-details.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit,Input,Output,EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
 @Component({
   selector: 'quotes-details',
   templateUrl: './quotes-details.component.html',
   styleUrls: ['./quotes-details.component.css']
 })
 export class QuotesDetailsComponent implements OnInit {
+  @Input()quote:Quote;
+  @Output()
+  ifSure=new EventEmitter<boolean>();
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
   }
+  DeleteQuote(complete:boolean){
+    this.ifSure.emit(complete);
+  }
+ 
+  
 
 }
