80482ba55c1d2b7ec3914bc12ecb8e4a8f7a414c
Moringa School <wangu.mwangi@student.moringaschool.com>
2022-04-10 01:39:57 +0300

    created component quote-form

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit,Output,EventEmitter } from '@angular/core';
 import { Quote } from '../quote';
 @Component({
-  selector: 'app-quote-form',
+  selector: 'quote-form',
   templateUrl: './quote-form.component.html',
   styleUrls: ['./quote-form.component.css']
 })
