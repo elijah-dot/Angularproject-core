1c768649698f3b809d9122e5f64decbc2df5f072
Moringa School <wangu.mwangi@student.moringaschool.com>
2022-04-10 00:45:50 +0300

    created component quotes-details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { QuoteComponent } from './quote/quote.component';
+import { QuotesDetailsComponent } from './quotes-details/quotes-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    QuoteComponent
+    QuoteComponent,
+    QuotesDetailsComponent
   ],
   imports: [
     BrowserModule
